a506fc8518a55bb193589b17941cf86756a3c189
Antonio <toni.cm@gmail.com>
2019-11-12 20:52:19 -0300

    Updates

---
+++ precache-manifest.e4051ca30d1d2b235de7e2628def7238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca33e4c8366adbef55e0373c747bb66",
+    "url": "/asana-tecton/index.html"
+  },
+  {
+    "revision": "f69701a5ead29a09b3ab",
+    "url": "/asana-tecton/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2d198ed29da4a8b75102",
+    "url": "/asana-tecton/static/js/2.8c37d210.chunk.js"
+  },
+  {
+    "revision": "f69701a5ead29a09b3ab",
+    "url": "/asana-tecton/static/js/main.ecb54296.chunk.js"
+  },
+  {
+    "revision": "582f50c5281b6207f813",
+    "url": "/asana-tecton/static/js/runtime-main.267accb0.js"
+  }
+]);
